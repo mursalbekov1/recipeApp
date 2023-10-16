c9e5016526ee0ae328a39ede392a9f443fe97346
mursalbekov1 <mursalbekov.merey@gmail.com>
2023-10-16 12:25:58 +0600

    chapter 6 migration

--- internal/data/models.go
+++ internal/data/models.go
@@ -5,20 +5,22 @@ import (
 )
 
 type Recipe struct {
-	ID          int64     `json:"id"`
-	Time        time.Time `json:"Time"`
-	Title       string    `json:"Title"`
-	Description string    `json:"Description"`
-	Runtime     Runtime   `json:"runtime,omitempty"`
-	Ingredients []string  `json:"Ingredients"`
-	Steps       []string  `json:"Steps"`
-	Author      int64     `json:"Author"`
+	ID            int64     `json:"id"`
+	Time          time.Time `json:"time"`
+	Title         string    `json:"title"`
+	Description   string    `json:"description"`
+	Runtime       Runtime   `json:"runtime,omitempty"`
+	Ingredients   []string  `json:"ingredients"`
+	Steps         []string  `json:"steps"`
+	Author        int64     `json:"author"`
+	Collaborators int64     `json:"collaborators"`
 }
 
 type Author struct {
-	ID       int64   `json:"id"`
-	Name     string  `json:"name"`
-	Email    string  `json:"email"`
-	Password string  `json:"-"`
-	Recipes  []int64 `json:"recipes"`
+	ID             int64   `json:"id"`
+	Name           string  `json:"name"`
+	Email          string  `json:"email"`
+	Password       string  `json:"-"`
+	Recipes        []int64 `json:"recipes"`
+	RecipeAccesses []int64 `json:"access_recipes"`
 }
